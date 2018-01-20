b82943140e00e8dd386366c7d033410ce2d33dfe
python3lover <35400564+python3lover@users.noreply.github.com>
2018-01-20 18:06:28 -0500

    Update engine.py

--- core/lib/engine.py
+++ core/lib/engine.py
@@ -3,6 +3,12 @@
 By @python3lover (GitHub and GitLab)
 '''
 class engine():
-  def __init__(self,path,command):
+  def __init__(self,configpath,path,command):
+    self.configpath = configpath
     self.path = path
     self.command = command
+    self.quit = False
+  def engine(self):
+    print('PythonCI')
+    while not self.quit:
+      pass#Not done
